b9c2e1e847bf9ce188b4642f6e1b1c84a663c5e8
yazidebouthouri28 <ybouthouri470@gmail.com>
2026-05-05 12:58:10 +0100

    AuthController

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
-/** Used when building with --configuration=production (see angular.json fileReplacements). */
 export const environment = {
   production: true,
-  apiUrl: 'https://app-backend1-djchfmg3bbcahjaj.austriaeast-01.azurewebsites.net/api',
+  apiUrl: 'https://app-backend1-djchfmg3bbcahjaj.austriaeast-01.azurewebsites.net',
   allowOfflineAuth: false
 };
